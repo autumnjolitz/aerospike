2bee3d6d013c03cf4a20ceb584f2215111ce6328
Ben Jolitz <Ben.Jolitz@acxiom.com>
2014-12-19 09:52:35 -0800

    Fix the license= field to be correct with LICENSE

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         'Topic :: Databases',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: BSD License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
